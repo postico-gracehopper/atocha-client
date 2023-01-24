5338651357bd667b706a1c7e8aba2703f2fd388a
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-24 15:25:31 -0600

    Implement basic recording button

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  StyleSheet, Text, View, StatusBar,
-} from 'react-native'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import Button from 'components/Button'
+import { Audio } from 'expo-av'
 import { colors } from 'theme'
 
 const styles = StyleSheet.create({
@@ -22,10 +21,47 @@ const styles = StyleSheet.create({
 
 const Details = ({ route, navigation }) => {
   const from = route?.params?.from
+
+  const [recording, setRecording] = React.useState()
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..')
+      await Audio.requestPermissionsAsync()
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      })
+
+      console.log('Starting recording..')
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY,
+      )
+      setRecording(recording)
+      console.log('Recording started')
+    } catch (err) {
+      console.error('Failed to start recording', err)
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..')
+    setRecording(undefined)
+    await recording.stopAndUnloadAsync()
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    })
+    const uri = recording.getURI()
+    console.log('Recording stopped and stored at', uri)
+  }
+
   return (
     <View style={styles.root}>
       <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>{`Details (from ${from})`}</Text>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
       <Button
         title="Go Back"
         color="white"
