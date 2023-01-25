b9a993dd2af829418efbe129714cd8611dc8e831
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-25 15:23:53 -0600

    API post/res working

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, View, StatusBar } from 'react-native'
+import { StyleSheet, View, Text, StatusBar } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 import { Audio } from 'expo-av'
 
 import Button from 'components/Button'
 import { colors } from 'theme'
+import { submitRecordingAsync } from '../../slices/recordingSlice'
 
 const Details = ({ route, navigation }) => {
   const from = route?.params?.from
   // MV the above seems navigation-based but isn't used?
+  const translatedText = useSelector((state) => state.recording.currentText)
+  // MV it wasn't working with useSelector(selectCurrentText) so I'm trying this,
+  // as it worked when the prior didn't in Postico Ski Shop
 
   const [recording, setRecording] = React.useState()
   const [recordings, setRecordings] = React.useState([])
+  const dispatch = useDispatch()
 
   async function startRecording() {
     try {
@@ -48,6 +54,7 @@ const Details = ({ route, navigation }) => {
     await Audio.setAudioModeAsync({
       allowsRecordingIOS: false,
     })
+
     const uri = recording.getURI()
 
     let updatedRecordings = [...recordings]
@@ -59,7 +66,19 @@ const Details = ({ route, navigation }) => {
       file: recording.getURI(),
     })
     setRecordings(updatedRecordings)
-    console.log('Recording stopped and stored at', uri)
+
+    try {
+      dispatch(submitRecordingAsync(uri))
+    } catch (err) {
+      console.error('Failed to send recording (POST where??)', err)
+      err.innerText = err.response
+        ? err.response.data.message
+        : 'Request Timed Out'
+    }
+
+    // console.log('Recording stopped and stored at', uri)
+    // console.log('UpdatedRecordings isss', updatedRecordings)
+    // console.log('Most recent file is here:', recording.getURI())
   }
 
   return (
@@ -75,6 +94,7 @@ const Details = ({ route, navigation }) => {
         backgroundColor={colors.pink}
         onPress={navigation.goBack}
       />
+      <Text>Your text is {translatedText}</Text>
     </View>
   )
 }
