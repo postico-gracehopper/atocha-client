61107a37ad9f83e241da2ffbf68b6bbfdfade96a
beq <blakebequette@gmail.com>
2023-01-28 17:15:55 -0800

    write translation handler promise

---
+++ src/pages/Details/translationHandler.js
@@ -0,0 +1,27 @@
+import { readAsStringAsync, EncodingType } from 'expo-file-system'
+import { io } from 'socket.io-client'
+
+export default function translationHandler({ uri, langSource, langTarget }) {
+  return new Promise((resolve, reject) => {
+    try {
+      readAsStringAsync(uri, {
+        encoding: EncodingType.Base64,
+      }).then((audioBase64) => {
+        const socket = io('http://127.0.0.1:3000') // need some ternary for TEST / LOCAL / PRODUCTION
+        socket.on('connect', () => {
+          socket.emit('audio', {
+            langSource,
+            langTarget,
+            audioData: audioBase64,
+            fileFormat: 'm4a',
+          })
+        })
+        socket.on('final-translation', (finalTranslation) => {
+          resolve(finalTranslation)
+        })
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
