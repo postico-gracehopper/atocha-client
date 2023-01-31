13505f42e2afa66422c5bdb8a6297d44071050af
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-31 16:15:59 -0600

    Add "loading" message when awaiting openAI vocab

--- src/pages/Vocab/Vocab.js
+++ src/pages/Vocab/Vocab.js
@@ -1,19 +1,29 @@
 import { useState, React } from 'react'
-import { View, Button, Text, StyleSheet } from 'react-native'
+import { View, Button, Text, ActivityIndicator, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
 
 const dummyData =
   'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.'
 
+const Loading = () => {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" />
+      <Text style={styles.loadingText}>Loading...</Text>
+    </View>
+  )
+}
+
 export default function Vocab() {
   const currInput = useSelector((state) => state.targetLanguage.input)
   const currOutput = useSelector((state) => state.targetLanguage.output)
   const [result, setResult] = useState()
+  const [isLoading, setIsLoading] = useState(false)
 
   async function onSubmit(event) {
     event.preventDefault()
-
+    setIsLoading(true)
     // First, generate the vocab list given the recent conversation and output language.
     try {
       const response = await axios({
@@ -33,24 +43,41 @@ export default function Vocab() {
         )
       }
       setResult(data.result)
+      setIsLoading(false)
     } catch (error) {
       console.error(error)
     }
   }
 
   return (
-    <View>
-      {/* <Button onPress={onSubmit} title="Press me" /> */}
-      <Button title="Press me" />
-      <View>
-        <Text onPress={onSubmit}>Testing</Text>
-        <Text style={styles.vocabList}>{result}</Text>
-      </View>
+    <View style={styles.container}>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <View>
+          {result ? <Text>{result}</Text> : <Text>No result yet</Text>}
+          <Button title="Submit" onPress={onSubmit} />
+        </View>
+      )}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    fontSize: 16,
+    marginTop: 10,
+  },
   vocabList: {
     padding: 20,
   },
