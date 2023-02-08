819afc14cbb3444d6d91964f7933d636d1f0f659
Meridith Viguet <meridith.viguet@gmail.com>
2023-02-07 18:04:35 -0600

    New vocab added to Phrasebook in real time

--- src/pages/Phrasebook/GetVocab.js
+++ src/pages/Phrasebook/GetVocab.js
@@ -4,37 +4,46 @@ import colors from '../../theme/colors'
 import { getAuth } from 'firebase/auth'
 import { db } from '../../../firebase'
 
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, collection, onSnapshot } from 'firebase/firestore'
 
 const GetVocab = () => {
-  const user = getAuth()
-  const currentUser = user.currentUser
-
   const [vocabWords, setVocabWords] = useState([])
+  const [userId, setUserId] = useState(null)
 
   const onGetVocab = async () => {
     console.log('In the getVocab async block :)')
-    if (currentUser) {
-      const userId = user.currentUser.uid
-      const querySnapshot = await getDocs(
-        collection(db, `User/${userId}/phrasebook`),
-      )
-      const vocabWords = querySnapshot.docs.map((doc) => {
-        return {
-          id: doc.id,
-          vocabWord: doc.data().vocabWord,
-          definition: doc.data().definition,
-        }
-      })
-      setVocabWords(vocabWords)
-    } else {
-      console.error('No user is signed in.')
-    }
+    const user = getAuth()
+    setUserId(user.currentUser.uid)
+    console.log('current user is', user.currentUser)
+
+    const unsub = onSnapshot(
+      collection(db, `User/${userId}/phrasebook`),
+      (querySnapshot) => {
+        console.log('Query path:', `User/${userId}/phrasebook`)
+
+        const vocabWords = querySnapshot.docs
+          .map((doc) => {
+            return {
+              id: doc.id,
+              vocabWord: doc.data().vocabWord,
+              definition: doc.data().definition,
+              dateAdded: doc.data().dateAdded,
+            }
+          })
+          .sort((a, b) => {
+            const dateA = a.dateAdded ? new Date(a.dateAdded) : 0
+            const dateB = b.dateAdded ? new Date(b.dateAdded) : 0
+            return -1 * (dateA - dateB)
+          })
+        setVocabWords(vocabWords)
+      },
+    )
+    return () => unsub()
   }
 
   useEffect(() => {
     onGetVocab()
-  }, [])
+  }, [userId, `User/${userId}/phrasebook`])
 
   return (
     <View style={styles.container}>
