b5909d4f7faf256f659f8ffd37f26af5c06443ab
Meridith Viguet <meridith.viguet@gmail.com>
2023-02-12 22:28:56 -0600

    Fixed broken vocab view

--- src/pages/Vocab/Vocab.js
+++ src/pages/Vocab/Vocab.js
@@ -321,7 +321,7 @@ const styles = StyleSheet.create({
   vocabContainer: {
     alignContent: 'flex-start',
     flexWrap: 'wrap',
-    paddingTop: 60,
+    paddingTop: 40,
     maxWidth: '100%',
   },
   vocabDefinition: {
